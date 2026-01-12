c8301c48acc521d4b1bfebdf46956c94cf81958f
pythonredpanda <Jamesajohnson013@gmail.com>
2026-01-12 16:34:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,4 @@ def score_clicker():
     return render_template("score_clicker.html")
 
 if __name__ == "__main__":
-    try:
-        app.run(debug=True, host="127.0.0.1", port=5000)
-    except Exception as e:
-        print("ERROR:", e)
-        input("Press Enter to close...")
+        app.run(debug=True)
